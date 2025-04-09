cda6ce455ffe1958035ad4d50c9644eb7c0e16b1
Jhuan Pablo <j.pablo.sena@gmail.com>
2025-04-08 21:10:33 -0300

    Add files via upload

---
+++ exercicios-vazios-main AREI/arrei/script.js
@@ -0,0 +1,7 @@
+/* var carro = ["Civic", "BMW", "Corsa", "Fusca"]
+
+alert(carros[0])
+ */
+/* var numeros = [[1,2,3],[4,5,6],[7,8,9]]
+
+alert(numeros[2][1]) */
---
+++ exercicios-vazios-main AREI/objetos/script.js
@@ -0,0 +1,24 @@
+/* var pessoa = {
+    nome: "Jhuan",
+    idade: 21,
+    bigode: true,
+    altura: 1.93
+}
+
+alert(`nome ${pessoa.nome}
+    Idade: ${pessoa.nome}
+    bigode: ${pessoa.nome}
+    altura: ${pessoa.nome}`)
+ */
+
+var carro = {
+    marca: "Ferrari",
+    modelo: "enzo",
+    ano: 2004,
+    portas: 4,
+    cor: "Branco",
+    preco: 10000000
+
+}
+
+alert(`Marca: ${carro.marca}`)
